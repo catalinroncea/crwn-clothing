b4a91bdc16bc34ee02152969a209efdcfbdfddd2
Roncea Catalin <catalin.roncea@gmail.com>
2021-01-15 13:37:47 +0200

    integrated firebase in application, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,21 @@
 import {Link} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo'/>
         </Link>
         <div className='options'>
-             <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
+            {currentUser ?
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                :
+                <Link to='/signin' className='option'>SIGN IN</Link>
+            }
         </div>
     </div>
 );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCODx7-vhERTGN80_vF-e6AV-5I9gtdPZk",
+    authDomain: "crwn-db-64052.firebaseapp.com",
+    projectId: "crwn-db-64052",
+    storageBucket: "crwn-db-64052.appspot.com",
+    messagingSenderId: "730169070430",
+    appId: "1:730169070430:web:9236df6da44f66acf0f5cf"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({promt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
