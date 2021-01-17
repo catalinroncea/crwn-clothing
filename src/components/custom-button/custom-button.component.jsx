8fe271570a3868ad1e1f8885919efae31d7665f3
Roncea Catalin <catalin.roncea@gmail.com>
2021-01-17 18:49:16 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,7 +1,13 @@
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in' : ''}`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button className={
+        `custom-button 
+        ${isGoogleSignIn ? 'google-sign-in' : ''}
+        ${inverted ? 'inverted' : ''}
+        `}
+        {...otherProps}
+    >
         {children}
     </button>
 );
